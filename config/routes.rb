bd5fdd5fe70b9ad83ad0d7298213f4ab76731d10
古田光流 <m1212r@furutahikariryuunoMacBook-Air.local>
2020-05-05 17:49:07 +0900

    Q&A用のmodel、controllerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'questions/show'
+  get 'questions/new'
+  get 'questions/edit'
+  get 'questions/destroy'
   devise_for :users,path:'',path_names: {sign_in:'login',sign_up:'up'}
   root 'tops#top'
   get 'index',to:'tops#index'
