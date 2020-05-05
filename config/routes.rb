191dca569c956cdb869c2fdbe02067a7471b5694
古田光流 <m1212r@furutahikariryuunoMacBook-Air.local>
2020-05-05 17:30:05 +0900

    ログイン機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users,path:'',path_names: {sign_in:'login',sign_up:'up'}
+  root 'tops#top'
+  get 'index',to:'tops#index'
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
